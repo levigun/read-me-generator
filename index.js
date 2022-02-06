7f8feacb225759ec3ff79d1a269d30101b21dda3
levigun <t.levina31@gmail.com>
2022-02-07 09:48:20 +1100

    added a filegenerator.js file

--- index.js
+++ index.js
@@ -1,7 +1,66 @@
-// TODO: Include packages needed for this application
+// node modules
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// array pf questions for user input
+const questions = () => {
+    return inquirer.prompt([
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your Github username?',
+        validate: (value)=>{ if(value){return true} else {return 'I need a value to continue'}}
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+        validate: (value)=>{ if(value){return true} else {return 'I need a value to continue'}}
+    },
+    {
+        type: 'input',
+        name: 'project-name',
+        message: 'What is your project\'s name?',
+        validate: (value)=>{ if(value){return true} else {return 'I need a value to continue'}}
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a short description about your project',
+        validate: (value)=>{ if(value){return true} else {return 'I need a value to continue'}}
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of license should your project have?',
+        choices: ['The MIT License', 'The GPL License' , 'Apache License', 'GNU License', 'N/A'],
+        validate: (value)=>{ if(value){return true} else {return 'I need a value to continue'}}
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'What command should be run to install dependencies?',
+        validate: (value)=>{ if(value){return true} else {return 'I need a value to continue'}}
+    },
+    {
+        type: 'input',
+        name: 'run',
+        message: 'What command should be run to run tests?',
+        validate: (value)=>{ if(value){return true} else {return 'I need a value to continue'}}
+    },
+    {
+        type: 'input',
+        name: 'about',
+        message: 'What does the user need to know about using the repo?',
+        validate: (value)=>{ if(value){return true} else {return 'I need a value to continue'}}
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'What does the user need to know about contributing to the repo?',
+    },
+ ]);
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
