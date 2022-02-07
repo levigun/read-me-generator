bb4508c31eda70cabb34309a1e8d7c50fabf33a9
levigun <t.levina31@gmail.com>
2022-02-07 14:01:55 +1100

    added licensing badges and made the readme generator to work

--- filegenerator.js
+++ filegenerator.js
@@ -1,58 +1,59 @@
 function fileGenerator(response) {
     var content = `# ${response.projectName}
 
-    ##Table of Content
-    * [Repository Content](#Repository Content)
-    * [Installation](#Installation)
-    * [Usage](#Usage)
-    * [Contribution Guidelines](#Contribution Guidelines)
-    * [Test Instruction](#Test Instructions)
-    * [Contact Information](#Contact Information)
-    * [Credits](#Credits)
-    * [License](#License)
-    
-    
-    ## Repository Content
-    ###### [Back to Table of Contents](#Table-of-Contents)
-    ${response.description}
-    
-    
-    ## User Story
-    ### As the Quiz Administrator
-    ###### [Back to Table of Contents](#Table-of-Contents)
-    1.   I a command-line application that accepts user input.
-    2.   I want to be prompted for information about my application repository.
-    3.   I want a high-quality, professional README.md is generated with the title of my project and sections entitled Table of Contents Description, Installation, Usage, Contribution Guidelines, Tests Instructions, Contact Information, and Licensing.
-    4.   I want my project name to display as the title of the README.
-    5.   I want a description, installation instructions, usage information, contribution guidelines, and test instructions; and then, this information is added to the sections of the README entitled Description, Installation, Usage, Contribution Guidelines, and Tests Instructions.
-    6.   I want to choose a license for my application from a list of options. 
-    7.   I want to enter my GitHub username; ant then this is added to the section of the README entitled Conctac Information, with a link to my GitHub profile.
-    8.   I want to enter my email address; and then this is added to the section of the README entitled Contact Information.
-    9.   I want to click on the links in the Table of Contents; and then, I am taken to the corresponding section of the README.
+${makeBadge(response.license)}
 
-    ## Installation
-    ${response.install}
+## Table of Content
+* [Repository Content](#repository-content)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contribution Guidelines](#contribution-guidelines)
+* [Test Instruction](#repository-content)
+* [Contact Information](#contact-information)
+* [Credits](#credits)
+* [License](#license)
 
-    ## Usage
-    ${response.usage}
 
-    ## Contribution Guidelines
-    ${response.contribution}
-    
-    ## Test Instructions
-    ${response.run}
-    
-    ## Contact Information
-    Email: ${response.email}
-    Github Username: [${response.username}](https://github.com/${response.username})
+## Repository Content
+### [Back to Table of Contents](#table-of-content)
+${response.description}
 
-    ## Credits
-    ${response.credits}
 
-    ## License
-    ${response.license}  
+## Installation
+${response.install}
+
+## Usage
+${response.usage}
+
+## Contribution Guidelines
+${response.contribution}
+
+## Test Instructions
+${response.run}
+
+## Contact Information
+Email: ${response.email}
+
+Github Username: [${response.username}](https://github.com/${response.username})
+
+## Credits
+${response.credits}
+
+## License
+${response.license}  
     `
     return content;
 };
+
+
+
+
+
+function makeBadge(license){
+    license = encodeURIComponent(license);
+
+    
+    return `![${license}](https://img.shields.io/badge/License-${license}-informational)`
+}
 
 module.exports = fileGenerator;
--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const fileGenerator = require("./filegenerator");
-const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user to answer
 const questions = [
@@ -11,56 +10,56 @@ const questions = [
         type: 'input',
         name: 'username',
         message: 'What is your Github username?',
-        validate: (value)=>{ if(value){return true} else {return 'I need a value to continue'}}
+        validate: (value)=> !!value || 'I need a value to continue'
     },
     {
         type: 'input',
         name: 'email',
         message: 'What is your email address?',
-        validate: (value)=>{ if(value){return true} else {return 'I need a value to continue'}}
+        validate: (value)=>!!value || 'I need a value to continue'
     },
     {
         type: 'input',
         name: 'projectName',
         message: 'What is your project\'s name?',
-        validate: (value)=>{ if(value){return true} else {return 'I need a value to continue'}}
+        validate: (value)=>!!value || 'I need a value to continue'
     },
     {
         type: 'input',
         name: 'description',
         message: 'Please write a short description about your project',
-        validate: (value)=>{ if(value){return true} else {return 'I need a value to continue'}}
+        validate: (value)=>!!value || 'I need a value to continue'
     },
     {
         type: 'list',
         name: 'license',
         message: 'What kind of license should your project have?',
         choices: ['The MIT License', 'The GPL License' , 'Apache License', 'GNU License', 'N/A'],
-        validate: (value)=>{ if(value){return true} else {return 'I need a value to continue'}}
+        validate: (value)=>!!value || 'I need a value to continue'
     },
     {
         type: 'input',
         name: 'install',
         message: 'What command should be run to install dependencies?',
-        validate: (value)=>{ if(value){return true} else {return 'I need a value to continue'}}
+        validate: (value)=>!!value || 'I need a value to continue'
     },
     {
         type: 'input',
         name: 'run',
         message: 'What command should be run to run tests?',
-        validate: (value)=>{ if(value){return true} else {return 'I need a value to continue'}}
+        validate: (value)=>!!value || 'I need a value to continue'
     },
     {
         type: 'input',
         name: 'credits',
         message: 'Please add some credits for your project:',
-        validate: (value)=>{ if(value){return true} else {return 'I need a value to continue'}}
+        validate: (value)=>!!value || 'I need a value to continue'
     },
     {
         type: 'input',
         name: 'usage',
         message: 'What does the user need to know about using the repo?',
-        validate: (value)=>{ if(value){return true} else {return 'I need a value to continue'}}
+        validate: (value)=>!!value || 'I need a value to continue'
     },
     {
         type: 'input',
